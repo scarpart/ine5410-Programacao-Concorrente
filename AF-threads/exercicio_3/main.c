1babb766019ea8065d875a4f16e89db121eaed48
scarpart <aenartzr@gmail.com>
2022-09-29 23:13:13 -0300

    assignment 3 done - code functional

--- AF-threads/exercicio_3/main.c
+++ AF-threads/exercicio_3/main.c
@@ -5,6 +5,17 @@
 #include <stdio.h>
 #include <pthread.h>
 
+/* definindo o struct que vai ser usado para passar
+todos os argumentos necessários à função thread */
+typedef struct allargs {
+    double* a;
+    double* b;
+    double* c;     
+    int n_loops;          // a_size / n_threads = n_loops
+    int* start;           // index do início do for na função thread
+    int* remainder_loops; // a_size % n_threads = remainder_loops
+} allargs;
+
 // Lê o conteúdo do arquivo filename e retorna um vetor E o tamanho dele
 // Se filename for da forma "gen:%d", gera um vetor aleatório com %d elementos
 //
@@ -15,6 +26,7 @@
 // v                                       v
 double* load_vector(const char* filename, int* out_size);
 
+void* thread(void* arg);
 
 // Avalia se o prod_escalar é o produto escalar dos vetores a e b. Assume-se
 // que ambos a e b sejam vetores de tamanho size.
@@ -60,9 +72,20 @@ int main(int argc, char* argv[]) {
     }
 
     //Calcula produto escalar. Paralelize essa parte
+    if (n_threads > a_size) n_threads = a_size;
+
+    int n_loops = a_size / n_threads;
+    int remainder_loops = a_size % n_threads;
+    int start = 0;
+    pthread_t all_threads[n_threads];
     double result = 0;
-    for (int i = 0; i < a_size; ++i) 
-        result += a[i] * b[i];
+    allargs std_args = { a, b, &result, n_loops, &start, &remainder_loops };
+
+    for (int i = 0; i < n_threads; ++i)
+        pthread_create(&all_threads[i], NULL, thread, (void*) &std_args);
+    
+    for (int j = 0; j < n_threads; j++)
+        pthread_join(all_threads[j], NULL); 
     
     //    +---------------------------------+
     // ** | IMPORTANTE: avalia o resultado! | **
@@ -74,4 +97,22 @@ int main(int argc, char* argv[]) {
     free(b);
 
     return 0;
+}
+
+void* thread(void* arg) {
+    allargs* args = (allargs*)arg;
+
+    /* essa variável armazena n_loops se não tiver remainder_loops,
+    caso tenha, ela vai armazenar n_loops+1 para distribuir os 
+    remainder_loops de forma igualitária para vários threads */
+    int temp = args->n_loops;
+    if (*(args->remainder_loops) > 0) {
+        temp++;
+        *(args->remainder_loops) -= 1;
+    }
+    
+    for (int i = *(args->start); i < temp + *(args->start); i++)
+        *(args->c) += args->a[i] * args->b[i];
+    *(args->start) += temp;
+    pthread_exit(NULL);
 }
