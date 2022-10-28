bb4bd5fba399e7ad55238c0e0358253b877809eb
scarpart <aenartzr@gmail.com>
2022-10-28 19:26:35 -0300

    added code for AF-semaforo, need to revise later

---
+++ AF-semaforo/exercicio_1/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
+#include <stdlib.h>
+
+FILE* out;
+sem_t sem_a, sem_b;
+
+void *thread_a(void *args) {
+    for (int i = 0; i < *(int*)args; ++i) {
+	//      +---> arquivo (FILE*) destino
+	//      |    +---> string a ser impressa
+	//      v    v
+        sem_wait(&sem_a);
+        fprintf(out, "A");
+        fflush(stdout);
+        sem_post(&sem_b);
+    }
+    return NULL;
+}
+
+void *thread_b(void *args) {
+    for (int i = 0; i < *(int*)args; ++i) {
+        sem_wait(&sem_b);
+        fprintf(out, "B");
+        fflush(stdout);
+        sem_post(&sem_a);
+    }
+    return NULL;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        printf("Uso: %s [ITERAÇÕES]\n", argv[0]);
+        return 1;
+    }
+    int iters = atoi(argv[1]);
+    srand(time(NULL));
+    out = fopen("result.txt", "w");
+
+    pthread_t ta, tb;
+    sem_init(&sem_a, 0, 1);
+    sem_init(&sem_b, 0, 1);
+
+    // Cria threads
+    pthread_create(&ta, NULL, thread_a, &iters);
+    pthread_create(&tb, NULL, thread_b, &iters);
+
+    // Espera pelas threads
+    pthread_join(ta, NULL);
+    pthread_join(tb, NULL);
+
+    //Imprime quebra de linha e fecha arquivo
+    fprintf(out, "\n");
+    fclose(out);
+  
+    return 0;
+}
